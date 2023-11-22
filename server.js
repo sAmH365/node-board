750784aa777b665c1dbc4faf6f1d84e7d0c7f95e
sAmH365 <npower5377@naver.com>
2023-11-22 22:40:23 +0900

    부트스트랩 css

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static(__dirname + '/public'))
+
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-    console.log(__dirname)
 })
 
 app.get('/about', (req, res) => {
