9861063569a3a0396481ad78f8d8e6ce3a9a5407
sAmH365 <npower5377@naver.com>
2023-11-23 18:27:50 +0900

    mongodb 연결 및 데이터 삽입

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(8080, () => {
-    console.log('http://localhost:8080 에서 서버 실행중')
+const { MongoClient } = require('mongodb')
+const url = 'mongodb://127.0.0.1:27017'
+new MongoClient(url).connect().then(client => {
+    console.log('DB연결 성공')
+    db = client.db('forum')
+
+    app.listen(8080, () => {
+        console.log('http://localhost:8080 에서 서버 실행중!')
+    })
+}).catch(err => {
+    console.log(err)
 })
 
 app.get('/', (req, res) => {
@@ -16,6 +25,7 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
+    db.collection('post').insertOne({title: (Math.floor(Math.random() * 2 + 1))})
     res.send('오늘 맑음')
 })
 
